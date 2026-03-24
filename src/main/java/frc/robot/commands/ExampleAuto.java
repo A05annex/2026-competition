edd1eecb7fa9d1da36bbc3fafd64250b9f915bc0
moralesxc <moralesxime2008@gmail.com>
2026-03-24 13:56:01 -0700

    Added brake mode and tuned values

--- src/main/java/frc/robot/commands/ExampleAuto.java
+++ src/main/java/frc/robot/commands/ExampleAuto.java
@@ -7,7 +7,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.CANDriveSubsystem;
 import frc.robot.subsystems.CANFuelSubsystem;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -21,12 +20,9 @@ public class ExampleAuto extends SequentialCommandGroup {
     // Drive backwards for .25 seconds. The driveArcadeAuto command factory
     // intentionally creates a command which does not end which allows us to control
     // the timing using the withTimeout decorator
-    new AutoDrive(driveSubsystem,-0.5,  0.0).withTimeout(.25),
+    new AutoDrive(driveSubsystem,-0.25,  0.0).withTimeout(2),
     // Spin up the launcher for 1 second and then launch balls for 9 seconds, for a
     // total of 10 seconds
-
-    //will hopefully wait 10 seconds before shooting fuel
-    new WaitCommand(10),
-    new Launch(ballSubsystem).withTimeout(3));
+    new LaunchSequence(ballSubsystem).withTimeout(4));
   }
 }
